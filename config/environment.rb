c83081a24fd14885737d4f3dedcd79320be6e033
inoue.yousuke.1991@gmail.com <vagrant@vagrant-ubuntu-trusty.vagrantup.com>
2017-03-15 13:36:30 +0000

    ローカルから

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.default_url_options = { host: 'localhost:3000' }
-config.action_mailer.delivery_method = :letter_opener_web
+# config.action_mailer.default_url_options = { host: 'localhost:3000' }
+# config.action_mailer.delivery_method = :letter_opener_web

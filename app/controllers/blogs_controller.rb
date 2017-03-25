fe2eac84b238f7f965a6f4634339aecaa8e6aa4b
inoue.yousuke.1991@gmail.com <vagrant@vagrant-ubuntu-trusty.vagrantup.com>
2017-03-25 08:31:52 +0000

    DIVE11課題前

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
   end
-  
+
   def new
     if params[:back]
       @blog = Blog.new(blogs_params)
@@ -12,47 +12,48 @@ class BlogsController < ApplicationController
       @blog = Blog.new
     end
   end
-  
+
   def create
     @blog = Blog.new(blogs_params)
     @blog.user_id = current_user.id
     if @blog.save
       redirect_to blogs_path, notice:'ブログを作成しました！'
+      NoticeMailer.sendmail_blog(@blog).deliver
     else
       render 'new'
     end
   end
-  
+
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     @blog = Blog.find(params[:id])
     if @blog.update(blogs_params)
       redirect_to blogs_path, notice:'ブログを編集しました！'
     else
-      
+
       render 'edit'
     end
   end
-  
+
   def destroy
     @blog = Blog.find(params[:id])
     @blog.destroy
   redirect_to blogs_path, notice:'ブログを削除しました！'
   end
-  
+
   def confirm
     @blog = Blog.new(blogs_params)
     render :new if @blog.invalid?
   end
-  
+
   private
     def blogs_params
       params.require(:blog).permit(:title, :content)
     end
-    
+
     def set_blog
       @blog = Blog.find(params[:id])
     end
